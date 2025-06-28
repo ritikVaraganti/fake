de702ce2bd1c519c0be844663057e33cf78c0988
ritikVaraganti <165842962+ritikVaraganti@users.noreply.github.com>
2025-06-27 20:14:55 -0700

    Update run_utils.py

--- run_utils.py
+++ run_utils.py
@@ -255,7 +255,7 @@ def get_main_ball(ball : Ball, match: Match = None) -> Ball:
     if match:
         ball.set_color(match)
 
-    if detections:
-        ball.detection = detections[0]
+    # if detections:
+    #     ball.detection = detections[0]
 
     return ball
