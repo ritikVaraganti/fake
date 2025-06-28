d0f19118e77477cd309d0326d1ec076e2fbfbad5
ritikVaraganti <165842962+ritikVaraganti@users.noreply.github.com>
2025-06-27 20:12:14 -0700

    Update run_utils.py

--- run_utils.py
+++ run_utils.py
@@ -197,6 +197,7 @@ def update_with_sanity_check(self, frame_length, frame_height, new_detection: no
     """
     if new_detection == None:
         print('new_detection=none')
+        return
     new_center = self.get_center(new_detection.absolute_points)
 
     dy = frame_length * 0.05
