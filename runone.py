ea3c8032b5b92fec6ab89f8a9e74ba940b383876
ritikVaraganti <165842962+ritikVaraganti@users.noreply.github.com>
2025-06-28 20:42:29 -0700

    Update runone.py

--- runone.py
+++ runone.py
@@ -119,18 +119,53 @@ buffer_size = 5
 prompt = "Evaluate player performance. Focus on key passes, tackles, fouls, and provide feedback."
 
 for i, frame in enumerate(video):
+    # Get Detections
     players_detections = get_player_detections(player_detector, frame)
     ball_detections = get_ball_detections(ball_detector, frame)
+    for det in ball_detections:
+        print("Bounding box points:", det.points)
+        print("Data dictionary:", det.data)
+        print("Confidence score:", det.scores)
+
+    
+
+
     detections = ball_detections + players_detections
-    coord_transformations = update_motion_estimator(motion_estimator, detections, frame)
-    player_track_objects = player_tracker.update(players_detections, coord_transformations)
-    ball_track_objects = ball_tracker.update(ball_detections, coord_transformations)
+
+    # Update trackers
+    coord_transformations = update_motion_estimator(
+        motion_estimator=motion_estimator,
+        detections=detections,
+        frame=frame,
+    )
+
+    player_track_objects = player_tracker.update(
+        detections=players_detections, coord_transformations=coord_transformations
+    )
+
+    ball_track_objects = ball_tracker.update(
+        detections=ball_detections, coord_transformations=coord_transformations
+    )
+    print(f'BALL TRACK OBJECTS{ball_track_objects}')
     player_detections = Converter.TrackedObjects_to_Detections(player_track_objects)
     ball_detections = Converter.TrackedObjects_to_Detections(ball_track_objects)
-    player_detections = classifier.predict_from_detections(player_detections, frame)
+    print(f'ball_detections 2: {ball_detections}')
+    player_detections = classifier.predict_from_detections(
+        detections=player_detections,
+        img=frame,
+    )
     height, width, _ = frame.shape
-    ball, _, _ = get_main_ball(ball_detections, height, width, (0, 0), (0, 0), match)
-    players = Player.from_detections(player_detections, teams)
+    # Match update
+    ball, prev_center, velocity = get_main_ball(
+        detections=ball_detections,
+        frame_length=height,
+        frame_height=width,
+        prev_center=prev_center,
+        velocity=velocity,
+        match=match
+    )
+    #print(f'ball: {ball}')
+    players = Player.from_detections(detections=players_detections, teams=teams)
     match.update(players, ball)
 
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
